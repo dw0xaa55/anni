7bbd081e016f886b2f481f509dd467d1004eb868
nphard <nphard@exocortex>
2025-05-04 01:08:04 +0200

    rewrite after long pause

--- anni.c
+++ anni.c
@@ -1,59 +1,46 @@
-/* DESCRIPTION: Artificial Neural Network Intelligence   */
-/* AUTHOR     : C. Huffenbach                            */
-/* VERSION    : 0.4                                      */
-/* DATE       : 08/2024                                  */
-
-/* TODO:                                                 */
-/*   - [X] think of trainingset architecture             */
-/*   - [X] create Network struct                         */
-/*   - [X] load trainingsets from file                   */
-/*   - [X] option to print networks                      */
-/*   - [X] implement feed_forward                        */
-/*   - [X] implement mean_square_error                   */
-/*   - [ ] implement finite_difference                   */
-/*   - [ ] save trained network to file                  */
-/*   - [ ] option to load trained networks from file     */
-
-
 #define ANNI_IMPLEMENTATION
 #include "anni.h"
 
-int main(int argc, char** argv){
-  (void) argc;
-  (void) argv;
+int main(){
+  srand(time(NULL));
 
-  // setup
-  Trainingset ts;
-  NeuralNetwork nn;
-  DataContainer inputs;
-  DataContainer outputs;
-  size_t topology[] = { 2,2,1};
+  // network topology
+  size_t topology[] = {2, 2, 1}; 
+#define NUM_LAYERS (sizeof(topology) / sizeof(topology[0]))
 
-  load_training_file(&ts,"trainingfile.ts");
-  print_trainingset(ts);
-  initialize_model(&nn, topology, GET_CONTAINER_SIZE(topology));
+  // initialize neural network.
+  NeuralNetwork *nn = createNeuralNetwork(topology, NUM_LAYERS);
 
-  // feed forward
-  get_inputs_from_trainingset(&inputs, ts);
-  get_outputs_from_trainingset(&outputs, ts);
-  print_container(inputs);
-  /* print_container(outputs); */
-  printf("-------------------------\n");
-  printf("feed forward debug output:\n");
+  // training data
+#define SAMPLE_AMOUNT 4
+  double input[SAMPLE_AMOUNT][2] = {{0.0, 0.0},
+				    {0.0, 1.0},
+				    {1.0, 0.0},
+				    {1.0, 1.0}};
+    
+  double output[SAMPLE_AMOUNT][1] = {{0.0},
+				     {1.0},
+				     {1.0},
+				     {0.0}};
 
-  /* nn.neurons[0] = inputs.data[2]; */
-  /* nn.neurons[1] = inputs.data[3]; */
-  /* feed_forward(&nn); */
-  /* print_model(nn); */
-
-  printf("error: %f\n", mean_square_error(nn, inputs, outputs));
+  // network training
+  double ANNI_learning_rate = 0.1;
+  double ANNI_epochs        = 1000000;
 
 
-  // clean up
-  free_container(inputs);
-  free_container(outputs);
-  free_model(nn);
-  free_trainingset(ts);
-  
+  for (int i = 0; i < ANNI_epochs; ++i){
+    size_t sample = i % SAMPLE_AMOUNT; // change
+    train(nn, input[sample], output[sample], ANNI_learning_rate);
+  }
+
+  // network check
+  printf("trained network:\n");
+  for (int i = 0; i < SAMPLE_AMOUNT; i++){
+    feedForward(nn, input[i]);
+    printf("Input: [%d, %d] -> Output: ", (int)input[i][0], (int)input[i][1]);
+    printOutput(nn);
+  }
+
+  freeNeuralNetwork(nn);
   return 0;
 }
--- anni.h
+++ anni.h
@@ -1,275 +1,207 @@
+/*
+ * Anni - Artificial Neural Network Intelligence
+ * Author : C. Huffenbach
+ * Date   : May 2025
+ * Version: 4.2 
+ * compiler string: gcc nn.c -lm -o nn
+ *
+ * TODO:
+ *   - [ ] save net to file
+ *   - [ ] load net from file
+ *   - [ ] load trainingdata from file
+ */
+
 #ifndef _ANNI_H_
 #define _ANNI_H_
 
+// includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
-
-typedef enum { false, true } bool;
+#include <time.h>
 
 typedef struct {
-  size_t input_size;
-  size_t output_size;
-  size_t samples;
-  size_t container_size;
-  size_t stride;
-  float* data;
-} Trainingset;
-
-typedef struct {
-  size_t size;
-  size_t stride;
-  size_t samples;
-  float* data;
-} DataContainer;
-
-typedef struct {
-  size_t  topology_size;
-  size_t  weights_size;
-  size_t  bias_size;
-  size_t  neurons_size;
-  size_t* topology;
-  float*  weights;
-  float*  bias;
-  float*  neurons;
+  // This structure represents the Neural Network
+  size_t num_layers;    // Total number of layers (input + hidden + output)
+  size_t *topology;     // Array that defines how many neurons per layer
+  double **neurons;     // Array of neuron activations for each layer
+  double **biases;      // Biases for neurons in each layer (except the input layer)
+  double ***weights;    // Weight matrices: weights[l][i][j] connects j-th neuron in (l-1)th layer to i-th neuron in layer l
 } NeuralNetwork;
 
-#define BUFFERSIZE 512
-#define GET_CONTAINER_SIZE(container) sizeof((container))/sizeof((container)[0])
+#define RANDOM_WEIGHT() ((double)rand() / (double)RAND_MAX * 2.0 - 1.0) // Macro to help initialize weights randomly between -1.0 and 1.0.
 
-void  load_training_file(Trainingset* trainingset, const char* filename);
-void  print_trainingset(Trainingset trainingset);
-void  free_trainingset(Trainingset trainingset);
-void  get_inputs_from_trainingset(DataContainer* container, Trainingset trainingset);
-void  get_outputs_from_trainingset(DataContainer* container, Trainingset trainingset);
-void  free_container(DataContainer container);
-void  print_container(DataContainer container);
-void  initialize_model(NeuralNetwork* model, size_t* topology, size_t topology_size);
-void  print_model(NeuralNetwork model);
-void  free_model(NeuralNetwork model);
-float randomize(float range_min, float range_max);
-float sigmoid(float x);
-void  feed_forward(NeuralNetwork* model);
-float mean_square_error(NeuralNetwork model, DataContainer inputs, DataContainer outputs);
-float finite_difference(NeuralNetwork model, float epsilon, float learningrate);
+double         sigmoid(double x);
+double         sigmoidDerivative(double output);
+NeuralNetwork* createNeuralNetwork(const size_t *topology, size_t num_layers);
+void           feedForward(NeuralNetwork *nn, double *input);
+void           printOutput(NeuralNetwork *nn);
+void           backPropagation(NeuralNetwork *nn, double *output, double learning_rate);
+void           train(NeuralNetwork *nn, double *input, double *output, double learning_rate);
+void           freeNeuralNetwork(NeuralNetwork *nn);
 
 #endif
 
 #ifdef ANNI_IMPLEMENTATION
+double sigmoid(double x)                { return 1.0 / (1.0 + exp(-x));   }
+double sigmoidDerivative(double output) { return output * (1.0 - output); }
 
-void load_training_file(Trainingset* trainingset, const char* filename){
-  FILE*  file = fopen(filename, "r");
-  char   buffer[BUFFERSIZE];
-  bool   data_is_active = false;
-  size_t data_counter   = 0;
-  char*  token          = "";
-
-  if(!file){
-    printf("Unable to open file \"%s\".\n", filename);
-    exit(1);
+NeuralNetwork*
+createNeuralNetwork(const size_t *topology, size_t num_layers){
+  // Create and initialize the neural network given the topology array and number of layers.
+  NeuralNetwork *nn = malloc(sizeof(NeuralNetwork));
+  nn->num_layers = num_layers;
+  nn->topology = malloc(num_layers * sizeof(int));
+  for(size_t i = 0; i < num_layers; ++i){
+    nn->topology[i] = topology[i];
   }
-  
-  while(fgets(buffer, BUFFERSIZE, file) != NULL) {
-    // skip commenset in file
-    if(buffer[0] == '#') continue;
-
-    // save values of input, output and samples to trainingset struct
-    sscanf(buffer, "input %zu\n",   &trainingset->input_size);
-    sscanf(buffer, "output %zu\n",  &trainingset->output_size);
-    sscanf(buffer, "samples %zu\n", &trainingset->samples);
-
-    // save data given by file into the trainingset struct
-    if(data_is_active && data_counter < trainingset->samples){
-      token = strtok(buffer, ",");
-      trainingset->data[data_counter * trainingset->stride] = (float)atof(token);
-      for(size_t i = 1; i < trainingset->stride; ++i){
-	trainingset->data[data_counter * trainingset->stride + i] = (float)atof(strtok(NULL, ","));
+    
+  // Allocate memory for neuron activations (one array per layer)
+  nn->neurons = malloc(num_layers * sizeof(double*));
+  for(size_t i = 0; i < num_layers; ++i){
+    nn->neurons[i] = calloc(topology[i], sizeof(double));
+  }
+    
+  // Allocate and initialize biases (starting from layer 1 as the input layer has no biases)
+  nn->biases = malloc(num_layers * sizeof(double*));
+  nn->biases[0] = NULL;  // Input layer
+  for(size_t i = 1; i < num_layers; ++i){
+    nn->biases[i] = malloc(topology[i] * sizeof(double));
+    for(size_t j = 0; j < topology[i]; ++j){
+      nn->biases[i][j] = RANDOM_WEIGHT();
+    }
+  }
+    
+  // Allocate and initialize weights between layers.
+  nn->weights = malloc(num_layers * sizeof(double**));
+  nn->weights[0] = NULL; // No weights for the input layer.
+  for(size_t i = 1; i < num_layers; ++i){
+    size_t neurons_in_current = topology[i];
+    size_t neurons_in_prev = topology[i-1];
+    nn->weights[i] = malloc(neurons_in_current * sizeof(double*));
+    for(size_t j = 0; j < neurons_in_current; ++j){
+      nn->weights[i][j] = malloc(neurons_in_prev * sizeof(double));
+      for(size_t k = 0; k < neurons_in_prev; ++k){
+	nn->weights[i][j][k] = RANDOM_WEIGHT();
       }
-      data_counter+=1;
-    }
-
-    // toggle data flag and calculate rest of control values
-    if(strcmp(buffer, "data\n") == 0){
-      data_is_active              = true;
-      trainingset->stride         = trainingset->input_size + trainingset->output_size;
-      trainingset->container_size = trainingset->stride * trainingset->samples;
-      trainingset->data           = malloc(sizeof(float) * trainingset->container_size);
     }
   }
-  fclose(file);
+  return nn;
 }
 
-void print_trainingset(Trainingset trainingset){
-  printf("Trainingset:\n");
-  printf("---------------\n");
-  for(size_t i = 0; i < trainingset.container_size; ++i){
-    if(i % trainingset.stride == 0 && i != 0) printf("\n");
-    printf("%f ", trainingset.data[i]);
+// Feedforward: calculates the output of the network given an input.
+void
+feedForward(NeuralNetwork *nn, double *input){
+  // set the input layer.
+  for(size_t i = 0; i < nn->topology[0]; ++i){
+    nn->neurons[0][i] = input[i];
   }
-  printf("\n---------------\n");
-}
-
-void free_trainingset(Trainingset trainingset){
-  free(trainingset.data);
-}
-
-void get_inputs_from_trainingset(DataContainer* container, Trainingset trainingset){
-  container->size    = trainingset.samples * trainingset.input_size;
-  container->stride  = trainingset.input_size;
-  container->samples = trainingset.samples;
-  container->data    = malloc(sizeof(float) * container->size);
-  for(size_t i = 0; i < trainingset.samples; ++i)
-    for(size_t j = 0; j < trainingset.input_size; ++j)
-      container->data[i*container->stride+j] = trainingset.data[i*trainingset.stride+j];
-}
-
-void get_outputs_from_trainingset(DataContainer* container, Trainingset trainingset){
-  container->size    = trainingset.samples * trainingset.output_size;
-  container->stride  = trainingset.output_size;
-  container->samples = trainingset.samples;
-  container->data    = malloc(sizeof(float) * container->size);  
-  for(size_t i = 0; i < trainingset.samples; ++i)
-    for(size_t j = trainingset.input_size; j < (trainingset.input_size+trainingset.output_size); ++j)
-      container->data[i * container->stride + (j - trainingset.input_size)] = trainingset.data[i*trainingset.stride+j];
-}
-
-void print_container(DataContainer container){
-  printf("Container:\n");
-  for(size_t i = 0; i < container.samples; ++i){
-    for(size_t j = 0; j < container.stride; ++j){
-      printf("%f ", container.data[i*container.stride+j]);
-    }
-    printf("\n");
-  }
-}
-
-void free_container(DataContainer container){
-  free(container.data);
-}
-
-void initialize_model(NeuralNetwork* model, size_t* topology, size_t topology_size){
-  model->topology_size = topology_size;
-
-  model->topology = malloc(sizeof(size_t) * model->topology_size);
-  for(size_t i = 0; i < model->topology_size; ++i)
-    model->topology[i] = topology[i];
-  
-  model->weights_size = 0;
-  for(size_t i = 0; i < model->topology_size - 1; ++i)
-    model->weights_size += model->topology[i] * model->topology[i+1];
-  model->weights = malloc(sizeof(float) * model->weights_size);
-  for(size_t i = 0; i < model->weights_size; ++i)
-    model->weights[i] = randomize(0.0f, 1.0f);
-
-  model->bias_size = model->topology_size - 1;
-  model->bias = malloc(sizeof(float) * model->bias_size);
-  for(size_t i = 0; i < model->bias_size; ++i)
-    model->bias[i] = randomize(0.0f, 1.0f);
-
-  model->neurons_size = 0;
-  for(size_t i = 0; i < model->topology_size; ++i)
-    model->neurons_size += model->topology[i];
-  model->neurons = malloc(sizeof(float) * model->neurons_size);
-  for(size_t i = 0; i < model->neurons_size; ++i)
-    model->neurons[i] = 0.0f;
-}
-
-void print_model(NeuralNetwork model){
-  printf("# model\n");
-  printf("topology_size %zu\n", model.topology_size);
-  printf("weights_size %zu\n",  model.weights_size);
-  printf("bias_size %zu\n",     model.bias_size);
-  printf("topology ");
-  for(size_t i = 0; i < model.topology_size; ++i)
-    printf("%zu ", model.topology[i]);
-  printf("\n");
-  printf("weights ");
-  for(size_t i = 0; i < model.weights_size; ++i)
-    printf("%f ",  model.weights[i]);
-  printf("\n");
-  printf("bias ");
-  for(size_t i = 0; i < model.bias_size; ++i)
-    printf("%f ",  model.bias[i]);
-  printf("\n");
-  printf("neurons ");
-  for(size_t i = 0; i < model.neurons_size; ++i)
-    printf("%f ",  model.neurons[i]);
-  printf("\n");
-}
-
-void free_model(NeuralNetwork model){
-  free(model.topology);
-  free(model.weights);
-  free(model.bias);
-  free(model.neurons);
-}
-
-float randomize(float range_min, float range_max){
-  return (float)rand() / (float)RAND_MAX * (range_max - range_min) + range_min;
-}
-
-float sigmoid(float x){
-  return 1.0f/(1.0f + expf(-x));
-}
-
-void feed_forward(NeuralNetwork* model){
-  size_t neuron_stride = 0;
-  size_t weight_stride = 0;
-  size_t layer_stride  = model->topology[0];
-  // creates two work matrices each step iterating through topology
-  for(size_t i = 0; i < model->topology_size - 1; ++i){
-    float work_matrix_1[model->topology[i]];
-    float work_matrix_2[model->topology[i] * model->topology[i+1]];
-
-    // populate matrices
-    for(size_t j = neuron_stride; j < neuron_stride + model->topology[i]; ++j)
-      work_matrix_1[j - neuron_stride] = model->neurons[j];
-    for(size_t j = weight_stride; j < weight_stride + model->topology[i] * model->topology[i+1]; ++j)
-      work_matrix_2[j-weight_stride] = model->weights[j];
     
-    // feed forward
-    size_t next_row = 0;
-    for(size_t j = 0; j < model->topology[i + 1]; ++j){
-      for(size_t k = 0; k < model->topology[i]; ++k)
-	model->neurons[layer_stride + j] += work_matrix_1[k] * work_matrix_2[next_row + k]; // multiply matrices
-      model->neurons[layer_stride + j] += model->bias[i];                                   // add bias to neuron
-      model->neurons[layer_stride + j] = sigmoid(model->neurons[layer_stride + j]);         // modify neuron with sigmoid activation function
-      next_row += model->topology[i];
+  // for every subsequent layer, compute the weighted sum and apply the sigmoid function.
+  for(size_t l = 1; l < nn->num_layers; ++l){
+    for(size_t j = 0; j < nn->topology[l]; ++j){
+      double sum = nn->biases[l][j];
+      for(size_t k = 0; k < nn->topology[l-1]; ++k){
+	sum += nn->weights[l][j][k] * nn->neurons[l-1][k];
+      }
+      nn->neurons[l][j] = sigmoid(sum);
     }
-
-    // stride increments
-    neuron_stride += model->topology[i];
-    weight_stride += model->topology[i] * model->topology[i + 1];
-    layer_stride  += model->topology[i+1];
   }
 }
 
-float mean_square_error(NeuralNetwork model, DataContainer input, DataContainer output){
-  size_t output_offset = model.neurons_size - model.topology[model.topology_size - 1];
+// Utility function to print the output layer neurons.
+void
+printOutput(NeuralNetwork *nn){
+  size_t output_layer = nn->num_layers - 1;
+  for(size_t i = 0; i < nn->topology[output_layer]; ++i){
+    printf("%f ", nn->neurons[output_layer][i]);
+  }
+  printf("\n");
+}
 
-  float error = 0.0f;
-  for(size_t i = 0; i < input.samples; ++i){
-    // step 1: put inputs in network
-    for(size_t j = 0; j < input.stride; ++j)
-      model.neurons[j] = input.data[i*input.stride + j];
+// The backpropagation routine calculates the error gradients (deltas) for each neuron,
+// and then updates the weights and biases accordingly.
+void
+backPropagation(NeuralNetwork *nn, double *output, double learning_rate){
+  size_t L = nn->num_layers;
     
-    // step 2: compute all neurons and thus outputs
-    feed_forward(&model);
-
-    // step 3: accumulate error for every output neuron and each trainingset
-    for(size_t j = 0; j < output.stride; ++j)
-      error += pow(model.neurons[output_offset + j] - output.data[i*output.stride + j], 2);
+  // Allocate an array of delta arrays (one per layer).
+  double **deltas = malloc(L * sizeof(double*));
+  for(size_t i = 0; i < L; ++i){
+    deltas[i] = calloc(nn->topology[i], sizeof(double));
   }
-  
-  return error/output.samples;
+    
+  // Calculate deltas for the output layer.
+  size_t output_layer = L - 1;
+  for(size_t i = 0; i < nn->topology[output_layer]; ++i){
+    double out   = nn->neurons[output_layer][i];
+    double error = out - output[i];  // For a simple squared error loss.
+    deltas[output_layer][i] = error * sigmoidDerivative(out);
+  }
+    
+  // Propagate deltas backwards for the hidden layers.
+  for(size_t l = L - 2; l > 0; --l){
+    for(size_t i = 0; i < nn->topology[l]; ++i){
+      double sum = 0.0;
+      for(size_t j = 0; j < nn->topology[l+1]; ++j){
+	sum += nn->weights[l+1][j][i] * deltas[l+1][j];
+      }
+      double output = nn->neurons[l][i];
+      deltas[l][i] = sum * sigmoidDerivative(output);
+    }
+  }
+    
+  // Update weights and biases using gradient descent.
+  for(size_t l = 1; l < L; ++l){
+    for(size_t i = 0; i < nn->topology[l]; ++i){
+      for(size_t j = 0; j < nn->topology[l-1]; ++j){
+	nn->weights[l][i][j] -= learning_rate * deltas[l][i] * nn->neurons[l-1][j];
+      }
+      // Update bias.
+      nn->biases[l][i] -= learning_rate * deltas[l][i];
+    }
+  }
+    
+  // Clean up delta arrays.
+  for(size_t i = 0; i < L; ++i){
+    free(deltas[i]);
+  }
+  free(deltas);
 }
 
-float finite_difference(NeuralNetwork model, float epsilon, float learningrate){
-  (void) model;
-  (void) epsilon;
-  (void) learningrate;
-  return 0.0f;
+// A simple training function that runs feedforward and backpropagation for a number of iterations.
+void
+train(NeuralNetwork *nn, double *input, double *output, double learning_rate){
+  feedForward(nn, input);
+  backPropagation(nn, output, learning_rate);
+}
+
+// Free all dynamically allocated memory of the neural network.
+void
+freeNeuralNetwork(NeuralNetwork *nn){
+  // Free neuron arrays.
+  for(size_t i = 0; i < nn->num_layers; ++i){
+    free(nn->neurons[i]);
+  }
+  free(nn->neurons);
+    
+  // Free biases.
+  for(size_t i = 1; i < nn->num_layers; ++i){
+    free(nn->biases[i]);
+  }
+  free(nn->biases);
+    
+  // Free weights.
+  for(size_t i = 1; i < nn->num_layers; ++i){
+    for(size_t j = 0; j < nn->topology[i]; ++j) {
+      free(nn->weights[i][j]);
+    }
+    free(nn->weights[i]);
+  }
+  free(nn->weights);
+    
+  free(nn->topology);
+  free(nn);
 }
 
 #endif
